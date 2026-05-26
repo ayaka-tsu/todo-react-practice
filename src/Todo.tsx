f534f9ac87227722ab1945495ac941c03e1acebe
Ayaka <peapocoron2353@gmail.com>
2026-05-26 16:06:09 +0900

    improve filter button UI

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -87,16 +87,37 @@ const Todo = () => {
             <button onClick={addTodo}>追加</button>
           </div>
           <div className="filter-buttons">
-            <button className="filter-btn" onClick={() => setFilter("すべて")}>
+            <button className={`filter-btn ${
+              filter==="すべて"
+              ? "active"
+              : ""
+            } `}
+            onClick={() => setFilter("すべて")}>
               すべて
             </button>
-            <button className="filter-btn" onClick={() => setFilter("未完了")}>
+            <button className={`filter-btn ${
+              filter==="未完了"
+             ? "active"
+            : ""
+            } `}
+
+              onClick={() => setFilter("未完了")}>
               未完了
             </button>
-            <button className="filter-btn" onClick={() => setFilter("着手中")}>
+            <button className={`filter-btn ${
+              filter==="着手中"
+            ? "active"
+            : ""
+            } `}
+              onClick={() => setFilter("着手中")}>
               着手中
             </button>
-            <button className="filter-btn" onClick={() => setFilter("完了")}>
+            <button className={`filter-btn ${
+              filter==="完了"
+                          ? "active"
+            : ""
+            } `}
+            onClick={() => setFilter("完了")}>
               完了
             </button>
           </div>
