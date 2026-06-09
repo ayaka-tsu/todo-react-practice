d3df3347a0f6327dfde5ec9a1547ea73f761b74b
Ayaka <peapocoron2353@gmail.com>
2026-06-10 00:36:51 +0900

    型共通化の修正

--- src/components/TodoDeleteButton.tsx
+++ src/components/TodoDeleteButton.tsx
@@ -1,39 +1,35 @@
-import type{Todo}from"../types/Todo";
+import type { Todo } from "../types/Todo";
 
-type TodoDeleteButtonProps={
+type TodoDeleteButtonProps = {
   todo: Todo;
-  todos:Todo[];
-  setTodos: React.Dispatch<
-    React.SetStateAction<Todo[]>
-    >;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
   setEditingId: React.Dispatch<React.SetStateAction<number | null>>;
-   deletedTodos: Todo[];   
-   setDeletedTodos:React.Dispatch<
-   React.SetStateAction<Todo[]>
->
+  deletedTodos: Todo[];
+  setDeletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 };
-const TodoDeleteButton=({
-    todo,
-   todos,
-   deletedTodos,
-   setTodos,
-   setDeletedTodos, 
-   setEditingId,
-}:TodoDeleteButtonProps)=>{
-    return(
-        <>
-                <button
-                  className="right-btn"
-                  onClick={() => {
-                    setDeletedTodos([...deletedTodos, todo]);
+const TodoDeleteButton = ({
+  todo,
+  todos,
+  deletedTodos,
+  setTodos,
+  setDeletedTodos,
+  setEditingId,
+}: TodoDeleteButtonProps) => {
+  return (
+    <>
+      <button
+        className="right-btn"
+        onClick={() => {
+          setDeletedTodos([...deletedTodos, todo]);
 
-                    setTodos(todos.filter((item) => item !== todo));
-                    setEditingId(null);
-                  }}
-                >
-                  削除
-                </button>
-        </>
-    );
+          setTodos(todos.filter((item) => item !== todo));
+          setEditingId(null);
+        }}
+      >
+        削除
+      </button>
+    </>
+  );
 };
 export default TodoDeleteButton;
